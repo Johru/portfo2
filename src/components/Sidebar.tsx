9578ddbdb05c7b33628cc24977abcd6d3f4bb770
Josef <johruza@gmail.com>
2023-06-15 20:59:01 +0200

    modal  added to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+"use client";
+
+import React, { useRef } from 'react';
 import styles from '../styles/layout.module.scss';
 
 export const Sidebar = () => {
+const dialog = useRef<HTMLDialogElement|null>(null)
+
+function openModal (){
+ dialog.current?.showModal();
+}
+
+function closeModal (){
+  dialog.current?.close();
+}
+
   return (
     <div className={styles.sidebar}>
       <h3>Contact me</h3>
-      <p>I am currently open to work, don't hesitate to contact me.</p>
+      <p>I am currently open to work, don`&apos;t hesitate to contact me.</p>
 
       <p></p>
       <p>johruza@gmail.com</p>
       <p>+420 799 991 186</p>
       <h3>CV</h3>
       <p>You can download my CVs here:</p>
-      <button>Donwload English CV</button>
-      <button>Donwload Czech CV</button>
-      <button>Donwload German CV</button>
+
+      <button onClick={openModal}>Download CV</button>
+      <dialog ref={dialog}>
+        This is a dialog.
+
+          <a href='/Josef_Hruza_en.pdf' download><button>Download English CV</button></a>
+      <a href='/Josef_Hruza_de.pdf' download><button>Download German CV</button></a>
+      <a href='/Josef_Hruza_cs.pdf' download><button>Download Czech CV</button></a>
+        <button onClick={closeModal}>Close Dialog</button>
+      </dialog>
+    
+     
     </div>
   );
 };
