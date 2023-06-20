ece0aac1e4f5d18bf47ef3ea0a89774b033adfef
Johru <74294571+Johru@users.noreply.github.com>
2023-06-20 21:02:47 +0200

    story updated

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,27 +1,42 @@
-import styles from '@/styles/page.module.scss'
+import styles from '@/styles/page.module.scss';
 
 export default function Contacts() {
-  return (<>
-    <div className={`${styles.grid} ${styles.g500}`}>
-     <p>Email: </p>
-     <p>johruza@gmail.com </p>
-     <p>Phone:</p>
-     <p>+420 799 991 186</p>
-     </div>
-     <div className={styles.grid}>
-     <p>Address:</p>
-     <p>Nad Pískovnou 1682/50</p>
-     <p></p>
-     <p>Praha 4</p>
-     <p></p>
-     <p>140 00</p>
-    </div>
-    <div className={styles.grid}>
-    <p>Github: </p>
-    <a className={styles.underline}>Githublink.com</a>
-    <p>LinkedIn: </p>
-    <a className={styles.underline}>LinkedIn.com</a>
-    </div>
+  return (
+    <>
+      <div className={`${styles.grid} ${styles.g500}`}>
+        <p>Email: </p>
+        <p>johruza@gmail.com </p>
+        <p>Phone:</p>
+        <p>+420 799 991 186</p>
+      </div>
+      <div className={styles.grid}>
+        <p>Address:</p>
+        <p>Nad Pískovnou 1682/50</p>
+        <p></p>
+        <p>Praha 4</p>
+        <p></p>
+        <p>140 00</p>
+      </div>
+      <div className={styles.grid}>
+        <p>Github: </p>
+        <a
+          className={styles.underline}
+          href="https://github.com/Johru?tab=repositories"
+          rel="noopener"
+          target="_blank"
+        >
+          https://github.com/Johru?tab=repositories
+        </a>
+        <p>LinkedIn: </p>
+        <a
+          className={styles.underline}
+          href="https://www.linkedin.com/in/josef-hrůza-58896813a/"
+          rel="noopener"
+          target="_blank"
+        >
+          https://www.linkedin.com/in/josef-hrůza-58896813a/
+        </a>
+      </div>
     </>
   );
 }
---
+++ src/app/story/page.tsx
@@ -0,0 +1,92 @@
+import styles from '@/styles/page.module.scss';
+
+export default function Story() {
+  return (
+    <div className={styles.g800}>
+      <div className={styles.article}>
+        <h2>Running up</h2>
+        <p>
+          How ironic are my teen years from today&apos;s perspective! When I was
+          just 17, I spent entire weeks fiddling with websites. I taught myself
+          basics of HTML and CSS and wrote them by hand in notepad. Back then,
+          Javascript was something you disabled to prevent websites from
+          pranking you. I wish someone had sat me down and explained to me how
+          learning it could be a very viable career path. Sadly, I never quite
+          realized that until 15 years later.
+        </p>
+
+        <p>
+          Being told multiple times that it is too late to learn programming, I
+          settled for a career in customer care, improving my communication
+          skills, learning to work from a command line, and most importantly,
+          receiving a steady stream of compliments for my good nature as a
+          mentor. All the good vibes kept me in my position for far longer than
+          they should have. Eventually though, I realized I long since hit a
+          skill ceiling.
+        </p>
+        <p>
+          Going forward, I had two choices. I could stay where I was and
+          continue for the coming decades of my career doing the same thing
+          every day and never learning anything of substance. Or I could take
+          the leap of faith and move to a completely different industry, one
+          where upskilling is as natural as breathing.
+        </p>
+        <p>
+          For a short while, the negative effects of stagnation were evident. I
+          am not built to stay in place for too long, my curiosity would smother
+          me if I tried! Despite loving my job as a mentor for years, I knew I
+          had to find a path on which I would never run out of new challenges.
+        </p>
+        <h2>Taking the Leap</h2>
+        <p>
+          At just the right moment, some of my work paid off. My second attempt
+          at building an application for tax calculation was accepted. My first
+          try was bloated, buggy, and really hard for users to understand. The
+          second was clean, simple, stable (at the cost of slightly reduced
+          scope) and came with solid documentation explaining every numbered
+          section of the app. As a result, the tool was officially endorsed by
+          my company and recommended as he go-to tool to calculate tax
+          differences in production across the reservations department.
+        </p>
+        <p>
+          All this of course was with no programming skills, using just MS Excel
+          in my free time, completely unpaid. What then, I asked myself, could I
+          eventually build with the full might and power of an actual
+          programming language?
+        </p>
+        <p>
+          To see where that road might lead, I left my job to join a fullstack
+          course at Green Fox Academy. Started from scratch with basics of
+          javascript, through many project and exams, all the way to the final
+          app we built as a team of students. I had the opportunity to work on
+          both the frontend and backend of an Angular app, build authorization
+          and authentication, and got my first pull requests rejected because of
+          tiny pedantic details I soon learned are more important than they
+          sound.
+        </p>
+        <p>
+          And here I am now, eager for an opportunity to gain proper and full
+          commercial experience in the field, yet unsure where to go on the ever
+          shifting market and how to finally get my foot in the door. One thing
+          is for sure though. I will never stop learning and exploring. Like a
+          shark in the water, I can&apos;t ever stop moving!
+        </p>
+      </div>
+
+      <div>
+        <h3>TLDR</h3>
+        <p>
+          If reading lengthy stories doesn&apos;t quite fit in your day, lets
+          summarize:
+        </p>
+        <p>
+          I couldn&apos;t bear the thought of not learning anything new anymore
+          and getting too comfortable. Thus I decided to switch fields to
+          software developent. My aim is to constantly learn, grow and keep a
+          finger on the pulse of modern development. I will probably never
+          settle for an easy corporate chair and stop pushing the boundaries.
+        </p>
+      </div>
+    </div>
+  );
+}
